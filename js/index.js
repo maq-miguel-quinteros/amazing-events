0d16f47cb2810714bbb215f5b022dc75a836a9bb
maq-miguel-quinteros <maq.miguel.quinteros@gmail.com>
2023-03-07 15:18:49 -0300

    task3

--- js/index.js
+++ js/index.js
@@ -1,24 +1,48 @@
 // MUESTRA LAS CATEGORIAS
-let categorias = data.events.reduce((acc,event)=>{
-    if(!acc.includes(event.category)){
+let categorias = data.events.reduce((acc, event) => {
+    if (!acc.includes(event.category)) {
         acc.push(event.category);
-        }
+    }
     return acc;
-    },[]);
+}, []);
 
 let showCategory = "";
-for (let categoria of categorias){
+for (let categoria of categorias) {
     showCategory += generarCategoria(categoria);
 }
 document.getElementById("paraCategoria").innerHTML = showCategory;
 
-for (let categoria of categorias){
-    document.getElementById(categoria).addEventListener("click", ()=> console.log(categoria))
+// MUESTRA LAS TARJETAS
+let selectCategorias = [];
+function mostrarTarjetas(selectCategorias) {
+    let showCards = "";
+    if (selectCategorias == ""){
+        for (let event of data.events){
+            showCards += generarTarjeta(event);
+        }
+    } else {
+        for (let event of data.events){
+            if (selectCategorias.includes(event.category)){
+                showCards += generarTarjeta(event);
+            }       
+        }
+    }   
+    document.getElementById("paraCards").innerHTML = showCards;
+}
+mostrarTarjetas(selectCategorias);
+
+
+for (let categoria of categorias) {
+    document.getElementById(categoria).addEventListener("click", () => {
+        if (!selectCategorias.includes(categoria)) {
+            selectCategorias.push(categoria)
+        } else {
+            selectCategorias.splice(selectCategorias.indexOf(categoria), 1);
+        }
+        mostrarTarjetas(selectCategorias);
+    })
 }
 
-// MUESTRA LAS TARJETAS 
-let showCards = "";
-for (let event of data.events) {
-    showCards += generarTarjeta(event);
-}
-document.getElementById("paraCards").innerHTML = showCards;
+
+
+
